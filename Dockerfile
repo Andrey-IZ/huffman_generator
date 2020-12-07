79f6149869e215587c850a6bd1375190911ef1a8
andrew <zaporozhtsev@grad.rnd>
2020-12-07 10:22:56 +0300

    Реализовано тестовое задание. (CLI интерфейс, без контейнера docker )

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.9-slim
-ADD . /app
-WORKDIR /app
-RUN pip install -r requirements.txt
-# ENV HUFFMAN_DIR_PATH="."
-
-ENTRYPOINT ["python", "huffman_generator.py"]
-CMD ["-o", "huffman_code.csv", "codes"]
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,5 @@
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.join(os.path.dirname(current_dir), 'app')
+sys.path.insert(0, parent_dir) 
 import huffman
